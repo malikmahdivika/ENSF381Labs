8721a946b5223a56862a38bba43b2221fb97f6ec
aasha25 <afsana.asha@ucalgary.ca>
2024-03-13 17:08:17 -0600

    Step C

--- lab08/products-app/src/components/ProductList.js
+++ lab08/products-app/src/components/ProductList.js
@@ -1,23 +1,32 @@
-import React, {useEffect, useState} from 'react';
-import {fetchProducts} from '../services/apiService';
-import {useProductsContext} from '../context/ProductsContext';
+import React, { useEffect, useState } from "react";
+import { fetchProducts } from "../services/apiService";
+import { useProductsContext } from "../context/ProductsContext";
+import { useNavigate } from "react-router-dom";
 
 const ProductList = () => {
-    const {products, setProducts} = useProductsContext();
+    const { products, setProducts } = useProductsContext();
+    const navigate = useNavigate();
 
-    useEffect (() => {
+    useEffect(() => {
         const getProducts = async () => {
             const data = await fetchProducts();
             setProducts(data.products);
         };
-
         getProducts();
     }, [setProducts]);
 
     return (
-        <div>
-            {products.map((product) => (
-                <div key={product.id}>{product.title}</div>
+        <div className="list-group">
+            {products.map((product, index) => (
+                <button
+                    key={product.id}
+                    className="list-group-item d-flex justify-content-between align-items-center hover-highlight"
+                    onClick={() => navigate(`/product/${product.id}`)}
+                >
+                    <span>
+                        {index + 1}. {product.title}
+                    </span>
+                </button>
             ))}
         </div>
     );
